ff408097aa1d6e159975d1e4d05f5e5c78d0d50e
Dvlpr Lya <108205878+Dvlprly@users.noreply.github.com>
2022-06-29 19:26:02 +0900

    Add files via upload

---
+++ [DAY 2].py
@@ -0,0 +1,78 @@
+#################################################################################################################################################
+### [DAY 2] ###
+#################################################################################################################################################
+# Data Types
+# Type Conversion
+# Operations
+# f-Strings
+
+
+#################################################################################################################################################
+### [CONCEPTS] ###
+#################################################################################################################################################
+# [Data Types]
+# String
+"Hello"
+print("Hello"[0])  # subscripting: pulling out a particular element
+# Integer
+123
+123_456_789  # for large integer = 123,456,789
+# Floatb
+3.14
+#Boolean
+True
+False
+
+
+# [len()]
+len("string")
+
+
+# [type()]
+type(abc)
+
+
+# [Type Conversion]
+str(abc)
+int(abc)
+float(abc)
+
+
+# [Operations]
+1 + 2
+1 - 2
+1 * 2
+1 / 2
+1 ** 2
+"str" + "str" 
+
+
+# [f-String]
+
+
+#################################################################################################################################################
+### [EXERCISES] ###
+#################################################################################################################################################
+# [ex 1 - https://replit.com/@appbrewery/day-2-1-exercise]
+two_digit_number = input("Type a two digit number: ")
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+print(first_number + second_number)
+# or
+two_digit_number = input("Type a two digit number: ")
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+result = int(first_number) + int(second_number)
+print(result)
+
+
+# [ex 2 - ]
+
+
+# [ex 3 - ]
+
+
+# [ex 4 - ]
+
+
+# [Final - ]
